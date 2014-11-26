473f87fd5f51c144adc177de76c5faeab6a2d164
Tim Wagner <t.wagner@techdivision.com>
2014-11-26 21:09:36 +0100

    Refactoring

--- src/var/www/phpinfo.php
+++
@@ -1,4 +0,0 @@
-<html>
-    <head><title>PHP info</title></head>
-    <body><pre><?php phpinfo(); ?></pre></body>
-</html>
--- src/var/www/test-cookie.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// header('Set-Cookie: track=978268624934537');
-
-foreach (getallheaders() as $name => $value) {
-    echo $name . ': ' . $value . '<br/>';
-}
