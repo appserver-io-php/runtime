45d1932feae7da40f311544e9fafd614c53cd798
Tim Wagner <t.wagner@techdivision.com>
2014-11-27 10:09:32 +0100

    Replace appserver. properties with runtime.

--- buildfiles/unix/scripts/build_openssl_dylib.sh
+++ buildfiles/unix/scripts/build_openssl_dylib.sh
@@ -7,11 +7,11 @@ mv openssl-$OPENSSL_VERSION openssl_i386
 tar -xvzf ../lib/openssl-$OPENSSL_VERSION.tar.gz
 mv openssl-$OPENSSL_VERSION openssl_x86_64
 cd openssl_i386
-./Configure darwin-i386-cc -shared --prefix=/opt/appserver
+./Configure darwin-i386-cc -shared --prefix=${runtime.compile.prefix}
 make
 cd ../
 cd openssl_x86_64
-./Configure darwin64-x86_64-cc -shared --prefix=/opt/appserver
+./Configure darwin64-x86_64-cc -shared --prefix=${runtime.compile.prefix}
 make
 cd ../
 lipo -create openssl_i386/libcrypto.1.0.0.dylib openssl_x86_64/libcrypto.1.0.0.dylib -output libcrypto.1.0.0.dylib
