e8a1edfc4bbb6870c53c557da0f6da08da976e8a
wick-ed <b.wick@techdivision.com>
2015-04-09 10:44:35 +0200

    added basic vagrant features

---
+++ buildfiles/linux/debian/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# install ant and other dependencies
+apt-get update
+apt-get -y install ant ant-contrib git autoconf automake bison make curl libxml2-dev libssl-dev pkg-config libbz2-dev libjpeg8-dev libpng-dev libfreetype6-dev libmcrypt-dev libmemcached-dev 
+libxpm-dev libc-client2007e-dev libpcre3-dev libpng12-dev libcurl4-openssl-dev libpng12-dev libevent-dev libev-dev libldap2-dev libicu-dev libxslt-dev;
