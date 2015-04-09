d84748f56ecb05ae8b48aa700790c7a231b53a5c
wick-ed <b.wick@techdivision.com>
2015-04-09 18:57:43 +0200

    added centos vagrant provisioning

---
+++ buildfiles/linux/centos/provision.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# install ant and other dependencies
+yum -y install ant ant-contrib git autoconf curl bison libxml2 libxml2-devel openssl-devel bzip2-devel libjpeg-turbo-devel libpng-devel freetype-devel libmcrypt-devel libmemcached-devel libXpm-devel pcre-devel libpng-devel libcurl-devel libevent-devel gcc-c++ openldap-devel libicu-devel libxslt-devel;
