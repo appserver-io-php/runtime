28d5f6a7fce445e6368278a6266661e9bb24e39a
wagnert <wagner_tim78@hotmail.com>
2016-04-11 21:46:36 +0200

    Switch to PHP 5.6.20 + Remove libmemcache and PECL extension memcached from build process

--- buildfiles/linux/centos/provision.sh
+++ buildfiles/linux/centos/provision.sh
@@ -2,7 +2,7 @@
 
 # install build dependencies
 yum -y install epel-release;
-yum -y install git autoconf curl bison libxml2 libxml2-devel openssl-devel bzip2-devel libjpeg-turbo-devel libpng-devel freetype-devel libmcrypt-devel libmemcached-devel libXpm-devel pcre-devel libpng-devel libcurl-devel libevent-devel gcc-c++ openldap-devel libicu-devel libxslt-devel;
+yum -y install git autoconf curl bison libxml2 libxml2-devel openssl-devel bzip2-devel libjpeg-turbo-devel libpng-devel freetype-devel libmcrypt-devel libXpm-devel pcre-devel libpng-devel libcurl-devel libevent-devel gcc-c++ openldap-devel libicu-devel libxslt-devel;
 
 # get the latest version of ant
 wget -q ${ant.download.url};
