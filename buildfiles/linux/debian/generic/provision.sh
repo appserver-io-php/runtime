9ce4c864cea73d9dc184b6fd2d774a9c3b7c8935
wickb <b.wick@techdivision.com>
2019-07-10 18:06:10 +0200

    removed Vagrant integration + properties handling for travis

--- buildfiles/linux/debian/generic/provision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# install ant and other dependencies
-apt-get update
-apt-get -y install ant autoconf make curl bison libxml2-dev libssl-dev pkg-config libbz2-dev libjpeg62-turbo-dev libfreetype6-dev libmcrypt-dev git-core libxpm-dev libc-client2007e-dev libpcre3-dev libcurl4-openssl-dev libsystemd-dev libpng-dev libevent-dev libev-dev libldap2-dev libicu-dev libxslt1-dev;
